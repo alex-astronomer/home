33fdcb06b3223e377bb7ec6c53eb0b5bb0854f2e
Alex Kennedy <alex.kennedy@astronomer.io>
2021-10-13 11:16:40 -0600

    make MQTTHandler handle -D directive when compiling to specify the name of the topics and hostname that should be used

--- lib/MQTTHandler.cpp
+++ lib/MQTTHandler.cpp
@@ -12,40 +12,47 @@ MQTTHandler::MQTTHandler(char* user, char* pass, Light &light) {
     this->lightController = light;
 }
 
-void MQTTHandler::publishStringToState(char* stateTopic, String statePayloadStr) {
-    int commandLength = statePayloadStr.length() + 1;
-    char returnState[statePayloadStr.length() + 1];
-    statePayloadStr.toCharArray(returnState, commandLength);
-    client.publish(stateTopic, returnState);
+void MQTTHandler::publishString(String topic, String payload) {
+    int payloadLen = payload.length() + 1;
+    char payloadArr[payloadLen];
+    payload.toCharArray(payloadArr, payloadLen);
+
+    int topicLength = topic.length() + 1;
+    char topicArr[topicLength];
+    topic.toCharArray(topicArr, topicLength);
+    
+    client.publish(topicArr, payloadArr);
 }
 
 void MQTTHandler::handleCommand(String command) {
-    char stateTopic[] = "dev/state";
+    // lightController.blink(4);
+    String stateTopic = deviceName + "/state";
     if (command == "ON") {
         lightController.on();
-        publishStringToState(stateTopic, command);
+        publishString(stateTopic, command);
     } else if (command == "OFF") {
         lightController.off();   
-        publishStringToState(stateTopic, command);
+        publishString(stateTopic, command);
     }
 }
 
 void MQTTHandler::handleBrightnessCommand(String brightness) {
     lightController.setBrightness(brightness.toInt());
-    publishStringToState("dev/brightness/state", brightness);
+    publishString(deviceName + "/brightness/state", brightness);
 }
 
 void MQTTHandler::handleRgbCommand(String csvRgb) {
     Rgb rgb = Rgb(csvRgb);
     lightController.setRgb(rgb.red, rgb.green, rgb.blue);
-    publishStringToState("dev/rgb/state", csvRgb);
+    publishString(deviceName + "/rgb/state", csvRgb);
 }
 
 void MQTTHandler::callback(char* topic, byte* payload, unsigned int length) {
+    // lightController.blink(14);
     std::map<String, std::function<void(String)>> callbackFnMap = {
-        {"dev", std::bind(&MQTTHandler::handleCommand, this, std::placeholders::_1)},
-        {"dev/brightness", std::bind(&MQTTHandler::handleBrightnessCommand, this, std::placeholders::_1)},
-        {"dev/rgb", std::bind(&MQTTHandler::handleRgbCommand, this, std::placeholders::_1)}
+        {deviceName, std::bind(&MQTTHandler::handleCommand, this, std::placeholders::_1)},
+        {deviceName + "/brightness", std::bind(&MQTTHandler::handleBrightnessCommand, this, std::placeholders::_1)},
+        {deviceName + "/rgb", std::bind(&MQTTHandler::handleRgbCommand, this, std::placeholders::_1)}
     };
     String payloadStr = Utils::bytePointerToString(payload, length);
     if (callbackFnMap.find(topic) != callbackFnMap.end()) {
@@ -59,11 +66,20 @@ void MQTTHandler::init(WiFiClient &net) {
     client.setCallback(
         std::bind(&MQTTHandler::callback, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3)
     );
+    String availableStateStr = deviceName + "/available";
+    int availableStateLen = availableStateStr.length() + 1;
+    char stateArr[availableStateLen];
+    availableStateStr.toCharArray(stateArr, availableStateLen);
     while (!client.connected()) {
-        if (!client.connect("dev", "alex", "assblood", "dev/available", 0, true, "0")) {
+        if (!client.connect(SPEC, "alex", "assblood", stateArr, 0, true, "0")) {
             delay(1000);
         }
     }
-    client.publish("dev/available", "1", true);
-    client.subscribe("dev/#");
+    client.publish(stateArr, "1", true);
+
+    availableStateStr = deviceName + "/#";
+    availableStateLen = availableStateStr.length() + 1;
+    char subscribeArr[availableStateLen];
+    availableStateStr.toCharArray(subscribeArr, availableStateLen);
+    client.subscribe(subscribeArr);
 }
