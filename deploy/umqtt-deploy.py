e2fde8f288049b8b1f107957af1e12880a6da4da
Alex Kennedy <alex.kennedy@astronomer.io>
2021-11-01 12:12:45 -0600

    add comments

--- deploy/umqtt-deploy.py
+++ deploy/umqtt-deploy.py
@@ -4,6 +4,13 @@ import yaml
 
 
 def replace_file_inplace(replace_this, with_this):
+    """
+    Replace string in place for a file
+
+    :param replace_this: string to replace
+    :param with_this: replacement string
+
+    """
     main_dir = "/Users/alexkennedy/Developer/home/upython"
     file = "main.py"
     full_path = f"{main_dir}/{file}"
@@ -14,6 +21,12 @@ def replace_file_inplace(replace_this, with_this):
 
 
 def get_py_files_in_dir(dir):
+    """
+    Recursively get path to all py files in a directory
+
+    :param dir: dir to search through recursively
+
+    """
     files_and_dirs = os.listdir(dir)
     if all([".py" in file for file in files_and_dirs]):
         return [f"{dir}/{file}" for file in files_and_dirs]
@@ -26,13 +39,19 @@ def get_py_files_in_dir(dir):
 
 
 if __name__ == '__main__':
+    # open config
     with open("/Users/alexkennedy/Developer/home/deploy/config.yaml", "r") as f:
         config = yaml.load(f, Loader=yaml.Loader)
+
+    # for bulb in config[ENV]
     for bulb_config in config["dev"]:
         device_name = bulb_config["name"]
         main_dir = "/Users/alexkennedy/Developer/home/upython"
         ip = bulb_config["ip"]
+        # get the paths to all py files in main_dir
         files = get_py_files_in_dir(main_dir)
+
+        # create the destination for the files on the uPython device
         dest_files = []
         for file in files:
             split_path = file.split('/')
@@ -40,6 +59,7 @@ if __name__ == '__main__':
             dest_files.append(f'/{"/".join(split_path[upython_i + 1:])}')
         zipper = zip(files, dest_files)
 
+        # after zipping the src and dst files together, upload each one to the proper uPython device
         for src_dst in zipper:
             dest_with_host = f"{ip}:{src_dst[1]}"
             if "main.py" in src_dst[0]:
